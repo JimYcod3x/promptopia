fb21eb540008c0e75005a57a490f3b8933c631db
JimYcod3x <jimycodex@gmail.com>
2023-07-11 01:59:30 +0800

    First Commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-console.log({
-    clientId: 
-    clientSecret: 
-})
+import User from '@models/user'
+import { connectToDB } from '@utils/database'
 
 const handler = NextAuth({
     providers: [
@@ -12,13 +10,45 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
+        
     ],
-    async seesion( { session }) {
 
-    },
-    async signIn( { profile }) {
+    callbacks: {
+        async session( { session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
 
+            session.user.id = sessionUser._id.toString()
+
+            return session
+        },
+        async signIn( { profile }) {
+            try {
+                
+                await connectToDB()
+                
+                const userExists = await User.findOne({
+                    email: profile.email
+                })
+    
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                
+                return true
+            } catch (error) {
+               console.log(error) 
+               return false
+            }
+        }
     }
+    
+    
 
 
     
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,19 +6,19 @@ import { useState, useEffect } from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = true
+  const { data: session } = useSession()
 
   const [ providers, setProviders ] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders()
 
       setProviders(response)
     }
 
-    setProviders()
+    setUpProviders()
   }, [])
 
   return (
@@ -34,9 +34,10 @@ const Nav = () => {
           <p className="logo_text">Promptopia</p>
       </Link>
 
+
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        { isUserLoggedIn ? (
+        { session?.user ? (
         <div className="flex gap-3 md:gap-5">
           <Link href="/create-prompt" className="black_btn">
             Create Post
@@ -48,7 +49,7 @@ const Nav = () => {
 
           <Link href="/profile">
             <Image
-              src="/assets/images/logo.svg"
+              src={ session?.user.image}
               width={37}
               height={37}
               alt="profile"
@@ -60,12 +61,14 @@ const Nav = () => {
         <>
           {providers &&
             Object.values(providers).map((provider) => (
+              
               <button
                 type="button"
-                key={ providers.name }
+                key={ provider.name }
                 onClick={() => signIn(provider.id)}
                 className="black_btn"
               >
+                
                 SignIn
               </button>
             )
@@ -77,10 +80,10 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        { isUserLoggedIn ? (
+        { session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               alt="profile"
@@ -123,7 +126,7 @@ const Nav = () => {
             Object.values(providers).map((provider) => (
               <button
                 type="button"
-                key={ providers.name }
+                key={ provider.name }
                 onClick={() => signIn(provider.id)}
                 className="black_btn"
               >
@@ -135,8 +138,6 @@ const Nav = () => {
             )
         }
       </div>
-
-      
     </nav>
   )
 }
